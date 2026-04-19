6fc8fb82d86960917c792700bc7771f131c1d199
Warren Ogu <w2j2oqgu5xr@users.noreply.github.com>
2026-04-19 12:21:35 +0800

    Default debug mode to true for easier local development

--- main.go
+++ main.go
@@ -17,7 +17,8 @@ var version = "dev"
 func main() {
 	var debugMode bool
 
-	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	// Default debug to true for local development convenience
+	flag.BoolVar(&debugMode, "debug", true, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
